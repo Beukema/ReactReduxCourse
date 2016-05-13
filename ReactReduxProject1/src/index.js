e212f68acaac299a122e3826091b8ba70674dff0
Nick Beukema <nick.beukema@gmail.com>
2016-05-13 09:05:50 -0400

    setup keys

--- ReactReduxProject1/src/index.js
+++ ReactReduxProject1/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import keys from '../keys';
+const API_KEY = keys().youtube_api;
+
+import SearchBar from './components/search_bar';
 
 
 const App = () => {
-  return <div>Hi!</div>
+  return <div>HI!</div>
 }
 
 
