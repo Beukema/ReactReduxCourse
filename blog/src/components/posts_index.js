e1f08cd2ee1d42e537ef6a8ccb9dad9add20a7d8
Nick Beukema <nick.beukema@gmail.com>
2016-05-26 12:32:41 -0400

    add redux-form

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -12,8 +12,10 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link to={`/posts/new`} className="btn btn-primary">Add a Post</Link>
+        </div>
         List of blog posts
-        <Link to={`/posts/new`}>New Post</Link>
       </div>
     );
   }
